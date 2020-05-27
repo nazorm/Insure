8f637fe0207ce5ffacd9ce6cc8f88f1ced762213
Nazorm <nnadoziechinazor@gmail.com>
2020-05-27 20:58:01 +0100

    initial commit

---
+++ insure.js
@@ -0,0 +1,17 @@
+/*function burgerbtn() {
+    
+    let x = document.querySelector(".nav-bar");
+    if (x.style.display === "block") {
+        x.style.diplay = "none";
+    } else {
+        x.style.display = "block";
+    }
+}*/
+function burgerbtn() {
+    let x = document.querySelector(".nav-bar");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
